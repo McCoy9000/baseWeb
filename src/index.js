cdbf93322180fd6e3958686e6e58237cd153d21a
Mikel Cuenca <mcmilbao@gmail.com>
2018-05-20 16:14:06 +0200

    Plugins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/styles.css';
 
 ReactDOM.render(
     <h1>Hello, you!</h1>,
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,58 @@
-module.exports = {
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    mode: "development",
+    entry: SRC_DIR + "/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
     module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+        rules: [
+            {
+                test: /\.css$/,
+                include: SRC_DIR,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                include: SRC_DIR,
+                use: [
+                  "babel-loader"
+                ]
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "app")
+        ],
+        alias: {
+            "modules": "app"
         }
-      ]
-    }
-  };
+    },
+    context: __dirname,
+    target: "web",
+    devServer: {
+        hot: true,
+    },
+    plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new NpmInstallPlugin()  
+    ]
+};
+
+module.exports = config;
