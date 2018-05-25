fb03e11bd9198208b30001442997434ffa422fef
Mikel Cuenca <mcmilbao@gmail.com>
2018-05-25 22:44:54 +0200

    eliminados tag div encerrando componentes

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import {Header} from './header';
 import {NavMenu} from './navmenu';
-import {Portfolio} from './portfolio';
-import {Resume} from './resume';
-import {Contact} from './contact';
-import {Blog} from './blog';
+import {Body} from './body';
 import {Footer} from './footer';
 
 export class App extends React.Component {
     render() {
         return (
             <div className='container'>
-                <div className='header'><Header /></div>
-                <div className='navMenu'><NavMenu /></div>
-                <div className='portfolio'><Portfolio /></div>
-                <div className='resume'><Resume /></div>
-                <div className='contact'><Contact /></div>
-                <div className='blog'><Blog /></div>
-                <div className='footer'><Footer /></div>
+                <Header />
+                <NavMenu />
+                <Body />
+                <Footer />
             </div>
         )
     }
--- src/js/body.js
+++ src/js/body.js
@@ -1,11 +1,20 @@
 import '../css/styles.css';
 import T from 'i18n-react';
 import React from 'react';
+import {Portfolio} from './portfolio';
+import {Resume} from './resume';
+import {Contact} from './contact';
+import {Blog} from './blog';
 
 export class Body extends React.Component {
     render() {
         return (
-            <div className={this.Component}></div>
+            <div className="body">
+                <Portfolio />
+                <Resume />
+                <Contact />
+                <Blog />
+            </div>
         )
     }
 }
