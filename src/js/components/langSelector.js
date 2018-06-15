b0793f61f5fa63c4f7a20d7c8de376f4f169efc3
Mikel <mcmilbao@gmail.com>
2018-06-15 10:20:15 +0200

    creado langSelector como component

---
+++ src/js/components/langSelector.js
@@ -0,0 +1,22 @@
+import '../../css/styles.css'
+
+import React from 'react'
+
+import {Button} from './common/button'
+import {FormattedMessage} from 'react-intl';
+import {store} from '../store/store'
+import {changeLang} from '../actions/langActions'
+
+function switchLanguage () {
+  store.dispatch(changeLang)
+}
+
+export class LangSelector extends React.Component {
+  render () {
+    return (
+      <div className='langSelector'>
+        <Button buttonTitle={<FormattedMessage id='langSelector' />} onClick={switchLanguage}/>
+      </div>
+    )
+  }
+}
